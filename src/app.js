94e5573264a8090c2dd0e0e52d04109f21e72d58
Himanshu-saini-18 <142436125+Himanshu-saini-18@users.noreply.github.com>
2026-03-30 08:01:17 +0530

    exploring git,post,delete request

--- src/app.js
+++ src/app.js
@@ -2,8 +2,19 @@ const express = require("express");
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+
+app.get("/user",(req,res)=>{
+    res.send({firstname:"himansh",lastname:"saini"})
+})
+app.post("/user",(req,res)=>{
+    console.log(req.body);
+    res.send("Data successfully saved to the database");
+})
+app.delete("/user",(req,res)=>{ 
+    res.send("deleted successfully");
+})
+app.use("/test",(req,res)=>{
+    res.send("hello from the server");
 })
 
 app.listen(3000, () => {
