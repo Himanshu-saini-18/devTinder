a5b9ff6c67ed3585a7ef731c1e2e86920c19d7ef
Himanshu-saini-18 <142436125+Himanshu-saini-18@users.noreply.github.com>
2026-04-12 20:42:17 +0530

    edit profile and logout api created

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,35 +1,31 @@
-
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 const userAuth = async (req, res, next) => {
- try{
-     //Read the token from the req.cookies
-  const cookies = req.cookies;
+  try {
+    //Read the token from the req.cookies
+    const cookies = req.cookies;
 
-  const { token } = cookies;
-  //validate the token
-  if (!token) {
-    throw new Error("Token is not valid!!!! ");
-  }
-  const decodedObj = await jwt.verify(token, "himansh@987");
+    const { token } = cookies;
+    //validate the token
+    if (!token) {
+      throw new Error("Token is not valid!!!! ");
+    }
+    const decodedObj = await jwt.verify(token, "himansh@987");
 
-  const { _id } = decodedObj;
- //find the user
-  const user = await User.findById(_id);
-  if (!user) {
-    throw new Error("User does not exist");
-  }
+    const { _id } = decodedObj;
+    //find the user
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User does not exist");
+    }
 
-  req.user=user;
-  next();
-
- }catch(err){
+    req.user = user;
+    next();
+  } catch (err) {
     res.status(404).send("Error: " + err.message);
- }
- 
+  }
 };
 module.exports = {
- 
   userAuth,
 };
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,11 +3,36 @@ const express = require("express");
 const profileRouter = express.Router();
 const jwt = require("jsonwebtoken");
 const { userAuth } = require("../../src/middlewares/auth");
-profileRouter.get("/profile", userAuth, async (req, res) => {
+
+const { validateEditProfileData } = require("../utils/validation");
+
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
 
     res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid edit request");
+    }
+    const loggedInUser = req.user;
+
+    // But this sis not good way  
+    // loggedInUser.firstName = req.body.firstName;
+    // loggedInUser.lastName = req.body.lastName;
+
+
+    Object.keys(req.body).forEach((key)=>(loggedInUser[key]=req.body[key]));
+       await loggedInUser.save();
+     
+   res.json({message: `${loggedInUser.firstName}, your profile updated successfuly`,data:loggedInUser});
+
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
